c445e326879e584452f351b7159c96af2d14f3af
Juan <juancalderonmicro@hotmail.com>
2025-09-12 20:20:29 -0500

    Changes made to the calculator

--- Calculator.cs
+++ Calculator.cs
@@ -8,4 +8,19 @@ class Calculator
     {
         return a + b;
     }
+
+    public int Subtract (int a, int b)
+    {
+        return a - b;
+    }
+    
+    public int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public var divisione(int a, int b)
+    {
+        return a / b;
+    }
 }
