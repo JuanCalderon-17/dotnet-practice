eef218e75c241d4ebe7d76ea7879ab6844d9f2da
Juan <juancalderonmicro@hotmail.com>
2025-10-13 23:06:32 -0500

    first exercise, kick off

--- stringPractice.cs
+++ stringPractice.cs
@@ -53,6 +53,12 @@ namespace dotnetPractice
             Console.WriteLine("First car: " + builder[0]);
         }
 
+
+
+        //1. Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive.
+        //  For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+
         
 
         
